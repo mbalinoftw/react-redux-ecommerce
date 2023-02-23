c0f3b5b1705beaff4dd0ff53a561d2c61b11e226
Mariano Baliño <marianobalinho@gmail.com>
2023-02-23 20:33:49 -0300

    add tailwindcss config

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-import App from "./App";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
+import App from "./App";
+import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
